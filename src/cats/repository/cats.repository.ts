3663c25214eb98169e8765ac8b537bd76d291c87
HolyWatter <qktjdtn97@icloud.com>
2023-06-19 22:55:46 +0900

    콘솔제거

--- src/cats/repository/cats.repository.ts
+++ src/cats/repository/cats.repository.ts
@@ -35,17 +35,13 @@ export class CatsRepository {
       password,
       name,
     });
-
     return result;
   }
 
   async findByIdAndUpdateImg(id: string, fileName: string) {
     const cat = await this.catModel.findById(id);
-
     cat.imgUrl = `http://localhost:8000/media/${fileName}`;
-
     const newCat = await cat.save();
-
     return newCat.readOnlyData;
   }
 
